37d06c3563d0099910179667d8c9323e4afc4180
Mich-31 <mikelediki22@gmail.com>
2024-01-05 10:41:25 +0100

    Fixed score sostenibilità

--- sustainameal/preprocessing.py
+++ sustainameal/preprocessing.py
@@ -1,12 +1,5 @@
 # preprocessing.py
 import pandas as pd
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize, wordpunct_tokenize
-from nltk.tag import pos_tag
-
-nltk.download('stopwords')
-nltk.download('averaged_perceptron_tagger')
 
 def remove_duplicate_titles(df):
     """
@@ -25,18 +18,3 @@ def remove_recipes_without_tags(df):
     :return: DataFrame with recipes without tags removed.
     """
     return df[df['tags'].notna() & (df['tags'] != '')]
-
-stop_words = set(stopwords.words('english'))
-
-# Funzione per rimuovere stopwords e aggettivi dalla colonna 'Food commodity ITEM'
-def remove_stopwords_and_adjectives(text):
-    stop_words = set(stopwords.words('english'))
-
-    tokens = wordpunct_tokenize(text)
-    tagged_words = pos_tag(tokens)
-    filtered_words = [word.replace('*', '') for word, tag in tagged_words if word.lower() not in stop_words and tag not in ['JJ', 'JJR', 'JJS']]
-    return ' '.join(filtered_words)
-
-def clean_ingredients_dataframe(df):
-    # Applica la rimozione delle stopwords e degli aggettivi alla colonna 'Food commodity ITEM'
-    return df['Food commodity ITEM'].apply(remove_stopwords_and_adjectives)
