feab441f5cfa86b2431ba43157f0dac8a8f0b719
PlaneB22 <47218421+petruzzellialessandro@users.noreply.github.com>
2025-01-13 15:12:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='HeASe',
+    name='sustainameal',
     version='0.1.0',
     description='A library to suggest more sustainable or healthy alternative recipes.',
     long_description=open('README.md').read(),
